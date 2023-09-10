a9d75db4ac781d3dc62b969de1666c5dc9022cbc
kashireddy <kashireddynaani9967@gmail.com>
2023-09-10 11:27:57 +0530

    updated script

--- 05-vaiables.sh
+++ 05-vaiables.sh
@@ -5,3 +5,8 @@ echo "Please enter your user name"
 read USERNAME # this is the variable where the value of the one user to enter
 
 echo "Username entered is : $USERNAME"
+
+echo "please eneter your password"
+
+read PASSSWORD
+echo "password enetered is : $PASSSWORD"
