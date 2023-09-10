0b0343bc0f5ba2c56b29f735e42e08ceddad252b
kashireddy <kashireddynaani9967@gmail.com>
2023-09-10 22:27:26 +0530

    updated script

--- 12.loops.sh
+++ 12.loops.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[om"
+if [ $USERID -ne o ]
+then
+    echo -e  " $R ERROR: please run this with root access $N"
+    exit 1
+fi
 # all arguments are in $@
 for i in $@
 do
