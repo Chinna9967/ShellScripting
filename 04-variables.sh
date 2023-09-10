c3e150d591169605df6cf6dde9965a69d27e9d52
kashireddy <kashireddynaani9967@gmail.com>
2023-09-10 11:06:45 +0530

    updated script

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# this is the comment, we need to develop a program that can do addition of two number
+
+NUMBER1=$1
+NUMBER2=$2
+
+# I need to run a command to add htese two numbers
+
+SUM=$((NUMBER1+NUMBER2))
