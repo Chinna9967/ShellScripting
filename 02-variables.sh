9c7324194eaa380dc222f45c27e7dd44122e7e11
kashireddy <kashireddynaani9967@gmail.com>
2023-09-10 10:56:47 +0530

    updated script

---
+++ 02-variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#shell script run the command inside dollar and braces and stores the o/p variable called DATE
+DATE=$(date)
+echo "Hello , this script is executed at timestamp: $DATE "
