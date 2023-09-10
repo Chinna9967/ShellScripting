77b2383a0aa481ce9b16d19c1d1ae7843ac231c4
kashireddy <kashireddynaani9967@gmail.com>
2023-09-10 11:02:32 +0530

    updated script

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "iam learning devops and git now onwards"
+
+person1=$1  # this is the first value from cl
+person2=$2  # this is the second value from cl
+
+echo "$person1: Hi $person2,Good morning"
+echo "$person2: Good morning $person1"
+echo "$person1: Hi $person2,how r u "
+echo "$person2: fine how r u $person1"
