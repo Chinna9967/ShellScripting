c00b284b3819d73c0577e313a43c7bfdb24d5156
kashireddy <kashireddynaani9967@gmail.com>
2023-09-10 11:41:09 +0530

    updated script

--- 05-vaiables.sh
+++ 05-vaiables.sh
@@ -4,6 +4,7 @@ echo "Please enter your user name"
 
 read -s USERNAME # this is the variable where the value of the one user to enter
 
+# this is only for validation, we should not print in the terminal
 echo "Username entered is : $USERNAME"
 
 echo "please eneter your password"
---
+++ 06-Array.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# this called array, used to store multiple values
+PERSONS=("Ramesh" "Suresh" "sachin")
+# inside array, wecan refer values with index which is starts from 0
+
+echo "First person is :${PERSONS[0]}"
